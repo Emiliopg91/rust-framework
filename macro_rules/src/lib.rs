72aa1465e02ab730fe8987ecf446f0dd314530c5
Emiliopg91 <ojosdeserbio@gmail.com>
2026-05-31 23:57:32 +0200

    Macros rules

--- macro_rules/src/lib.rs
+++ macro_rules/src/lib.rs
@@ -1,44 +1,39 @@
 #[macro_export]
 macro_rules! debug {
-    ($name:expr, $fmt:expr $(, $args:tt)*) => {{
-        fwkarq::logger::provider::Provider::get_logger($name).debug(
-            format!($fmt $(, $args)*)
-        );
+    ($name:expr, $($arg:tt)*) => {{
+        fwkarq::logger::provider::Provider::get_logger($name)
+            .debug(format!($($arg)*));
     }};
 }
 
 #[macro_export]
 macro_rules! info {
-    ($name:expr, $fmt:expr $(, $args:tt)*) => {{
-        fwkarq::logger::provider::Provider::get_logger($name).info(
-            format!($fmt $(, $args)*)
-        );
+    ($name:expr, $($arg:tt)*) => {{
+        fwkarq::logger::provider::Provider::get_logger($name)
+            .info(format!($($arg)*));
     }};
 }
 
 #[macro_export]
 macro_rules! warning {
-    ($name:expr, $fmt:expr $(, $args:tt)*) => {{
-        fwkarq::logger::provider::Provider::get_logger($name).warning(
-            format!($fmt $(, $args)*)
-        );
+    ($name:expr, $($arg:tt)*) => {{
+        fwkarq::logger::provider::Provider::get_logger($name)
+            .warning(format!($($arg)*));
     }};
 }
 
 #[macro_export]
 macro_rules! error {
-    ($name:expr, $fmt:expr $(, $args:tt)*) => {{
-        fwkarq::logger::provider::Provider::get_logger($name).error(
-            format!($fmt $(, $args)*)
-        );
+    ($name:expr, $($arg:tt)*) => {{
+        fwkarq::logger::provider::Provider::get_logger($name)
+            .error(format!($($arg)*));
     }};
 }
 
 #[macro_export]
 macro_rules! critical {
-    ($name:expr, $fmt:expr $(, $args:tt)*) => {{
-        fwkarq::logger::provider::Provider::get_logger($name).critical(
-            format!($fmt $(, $args)*)
-        );
+    ($name:expr, $($arg:tt)*) => {{
+        fwkarq::logger::provider::Provider::get_logger($name)
+            .critical(format!($($arg)*));
     }};
 }
