155ba6b524f1ac348980862cf52b620115455ee7
Emiliopg91 <ojosdeserbio@gmail.com>
2026-06-01 15:54:05 +0200

    Features

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,11 +1,23 @@
-pub mod event_bus;
 pub mod logger;
-pub mod rest_client;
-pub mod serialization;
-pub mod settings;
-pub mod shell;
-pub mod translator;
 pub mod utils;
+
+#[cfg(feature = "event_bus")]
+pub mod event_bus;
+
+#[cfg(feature = "rest_client")]
+pub mod rest_client;
+
+#[cfg(feature = "serialization")]
+pub mod serialization;
+
+#[cfg(feature = "settings")]
+pub mod settings;
+
+#[cfg(feature = "shell")]
+pub mod shell;
+
+#[cfg(feature = "translation")]
+pub mod translator;
 
 #[cfg(feature = "macros")]
 pub use fwkarq_macros::*;
